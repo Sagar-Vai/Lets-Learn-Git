c9055358b313d94be0e5a27e70b0b82542844033
Sagar_Vaishnnav <sagar78197@gmail.com>
2021-12-22 14:53:24 +0530

    add sum method

--- sum.java
+++ sum.java
@@ -2,6 +2,11 @@ public class Main{
      public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        System.out.println("the sum of two values"+a+b);
+        
+        int res = sum(a,b);
+        System.out.println("the sum of two values"+res);
+    }
+    public static int sum(int x,int y){
+        return x+y;
     }
 }
