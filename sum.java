3c9b946428751e84c556ab4f2fe60d2da31555c9
Sagar_Vaishnnav <sagar78197@gmail.com>
2021-12-22 13:04:06 +0530

    initial commit

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
-class Main{
+public class Main{
      public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        System.out.println(a+b);
+        System.out.println("the sum of two values"+a+b);
     }
 }
